04f47931ccff342dc2a78595a35ca7628c4b2236
Hesen_ch75@itstep.edu.az <Hesen_ch75@itstep.edu.az>
2023-07-21 16:42:21 +0400

    Add project files.

---
+++ bank.sistem/Model/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank.sistem.Model
+{
+    public class Class1
+    {
+        
+
+
+    }
+}
